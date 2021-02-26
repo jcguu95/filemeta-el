777c9a9a980bb4e368a4cb0382281bee00e2d625
jcguu95 <jcguu95@gmail.com>
2021-02-25 19:04:15 -0600

    took away the old db

--- filemeta.el
+++ filemeta.el
@@ -10,17 +10,6 @@
        :tags '(college math career)
        :hists nil))
 
-(setq filemeta-eg2
-      (make-filemeta
-       :path "~/test-image/2.jpg"
-       :comments '("I dislike this photo.")
-       :tags '(college math career)
-       :hists nil))
-
-(setq *filemeta-db* nil)
-(setq *filemeta-db-path* "/tmp/filemeta.db")
-(push filemeta-eg1 *filemeta-db*)
-(push filemeta-eg2 *filemeta-db*)
 (mkdir (setq *filemeta-root-dir* "/tmp/filemeta") t)
 
 ;;;;;; functions ;;;;;;
@@ -78,35 +67,7 @@
 (defun filemeta-update-history ())
 (defun filemeta-show-history ())
 
-;; db
-(defun filemeta-add (filemeta)
-  (cl-pushnew filemeta *filemeta-db*))
-;(defun filemeta-add-interactive ())
-(defun filemeta-remove (filemeta)
-  (setf *filemeta-db*
-        (remove filemeta *filemeta-db*)))
-;(defun filemeta-remove-interactive ())
-
-;; I/O
-;; TODO eventually i want to store each filemeta in its own file by hashing
-(defun filemeta-write-db ()
-  "Overwrite *FILEMETA-DB-PATH* with a READable form of
-  *FILEMETA-DB*."
-  (write-region (prin1-to-string *filemeta-db*)
-                nil *filemeta-db-path*))
-;(defun filemeta-safely-write-db (path))
-
-(defun filemeta-read-db ()
-  "Read the PATH and expect a list of FILEMETAs."
-  (setq *filemeta-db*
-        (read (f-read-text *filemeta-db-path* 'utf-8))))
-;(defun filemeta-safely-read-db (path))
-
 ;; dired
-(defun filemeta-dired-do-at-point (operation)
-  "Extract filepath at point, and destructively apply OPERATION
-  on all filemetas in *FILEMETA-DB*.")
-
 (defun filemeta-dired-get-file-at-point ()
   "Get the file at point."
   (let ((file (if (derived-mode-p 'dired-mode)
@@ -122,13 +83,6 @@
     files))
 
 
-;;; TODO FIXME working on interactive add meta
-;;; (setq filemeta-eg1
-;;;       (make-filemeta
-;;;        :path "~/test-image/1.jpg"
-;;;        :comments '("I love this photo.")
-;;;        :tags '(college math career)
-;;;        :hists nil))
 
 (defun filemeta-add-tag-to-file (file tag)
   "TAG is expected to be a symbol. FILE is expected to be a
