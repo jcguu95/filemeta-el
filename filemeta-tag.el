03db2a0317e7454e9cece3e9c26fee20a59134d4
jcguu95 <jcguu95@gmail.com>
2021-02-26 08:09:24 -0600

    updated and tested tags adder.. more functional and use more utils

--- filemeta-tag.el
+++ filemeta-tag.el
@@ -40,37 +40,42 @@ copy. FILEMETA untouched."
 ;;; add tags
 
 (defun filemeta-add-tag (filemeta symbol)
-  ;; e.g. (filemeta-add-tag filemeta-eg1 'math)
-  (if (symbolp symbol)
-      (cl-pushnew symbol (filemeta-tags filemeta))
-    (error "SYMBOL must be a symbol.")))
+  "Functionally add SYMBOL to the tag slot of FILEMETA, without
+touching FILEMETA."
+  (let ((result (copy-filemeta filemeta)))
+    (if (symbolp symbol)
+        (cl-pushnew symbol (filemeta-tags result))
+      (error "SYMBOL must be a symbol."))
+    result))
 
 (defun filemeta-add-tag-to-file (file tag)
-  "TAG is expected to be a symbol. FILE is expected to be a
-pathname. .. ETC."
-  (let* ((abs-file (file-truename file))
-         (data-file (ensure-file-exists (concat *filemeta-root-dir* abs-file))) ;; deconstr!
-         (content (f-read-text data-file 'utf-8))
-         (data (ignore-errors (read content))))
-    ;; if data is empty.. create a new filemeta for it
-    ;; otherwise, add the tag for it.
-    (if (eq data nil)
-        (setf data (make-filemeta :path abs-file
-                                  :comments nil
-                                  :tags (list tag)
-                                  :hists nil))
-      (filemeta-add-tag data tag))
-    ;; and then write the result back
-    (write-region (prin1-to-string data) nil data-file)))
+  "Deconstructively push a single symbol TAG into the tags-slot
+of the filemeta of a single file FILE."
+  (let* ((data-file (filemeta-data-for-file--ensured file))
+         (filemeta (filemeta-filemeta-from-data-file data-file)))
+    (filemeta-write-filemeta-to-data-file
+     (if (eq filemeta nil)
+         (make-filemeta :path abs-file :comments nil
+                        :tags (list tag) :hists nil)
+       (filemeta-add-tag filemeta tag))
+     data-file)))
 
-(defun filemeta-add-tag-to-file-at-point ()
-  ;; TODO add doc string
+(defun filemeta-tokenize (str)
+  "A utility that tokenize the input string STR into a list of
+symbols."
+  (mapcar #'intern (split-string (s-collapse-whitespace str))))
+
+(defun filemeta-add-tags-to-file-at-point ()
+  "In dired-mode, ask the user to input a string STR. Tokenize
+the string to a list of tags. Deconstructively add the tags to
+the data file of the file at point."
   ;; TODO add to multiple files?
   (interactive)
-  (let* ((tags-string (ivy-read "Enter tags: " nil)) ;; TODO read candidates from a tag db
-         (tags (mapcar #'intern (split-string (s-collapse-whitespace tags-string))))) ;; tokenize the tags
+  (let* ((str (ivy-read "Add tags: " nil)) ;; TODO read candidates from a tag db
+         (tags (filemeta-tokenize str))
+         (file (dired-get-filename)))
     (loop for tag in tags do
-          (filemeta-add-tag-to-file (dired-get-filename) tag))))
+          (filemeta-add-tag-to-file file tag))))
 
 ;;; remove tags
 
