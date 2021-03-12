24ae0419dc9bcd92f3825fad62c353b6663e8e94
jcguu95 <jcguu95@gmail.com>
2021-03-12 14:15:44 -0600

    wrote some functions for the new paradigm

---
+++ filemeta-root.el
@@ -0,0 +1,53 @@
+;; TODO under construction
+
+(defun filemeta-wheres-root (path)
+  "Return the filemeta-root for PATH."
+
+  ;; Search for directory ".filemeta" in the directory of PATH.
+  ;; Return the path of ".filemeta" if any. Otherwise, go to the
+  ;; mother directory and do it again, and again and again until
+  ;; it hits "/". In this case, return nil.
+
+  )
+
+(defun filemeta-path-hash (path)
+  "If PATH is a regular file, return the md5sum for its content.
+  Otherwise, return nil."
+  (flet ((md5sum (file)
+                 (with-demoted-errors "Error: %S" ;; TODO what does this do exactly?
+                   (and (file-exists-p file)
+                        (with-temp-buffer
+                          (insert-file-contents file)
+                          (md5 (buffer-string)))))))
+    (when (file-regular-p path)
+      (md5sum path))))
+
+(defun filemeta-hash-dir (file)
+  "Return the path to the filemeta for the hash of FILE."
+  (let ((hash (filemeta-path-hash file))
+        (root (filemeta-wheres-root file)))
+    (concat root hash)))
+
+(defun filemeta-hash-file (file)
+  "Return the path to the filemeta database for the hash of
+FILE."
+  (concat (filemeta-hash-dir file) ".db.el"))
+
+(defun filemeta-read-filemeta (file)
+  ;; FIXME conflicts with the old one?
+  ;; expect a FILEMETA to be read.
+  ""
+  (read (filemeta-hash-file file)))
+
+(defun filemeta-write-filemeta (filemeta file)
+  ;; FIXME conflicts with the old one?
+  ""
+  (if (filemeta-p filemeta)
+      ;; (WRITE filemeta TO (filemeta-hash-file file))
+      (error "Type error.")))
+
+(defun filemeta-tag-file (tag file)
+  "Expect TAG to be a symbol. Add tag to the filemeta of FILE,
+and write the updated filemeta to the hash-file for FILE."
+  ;; TODO ..
+  )
